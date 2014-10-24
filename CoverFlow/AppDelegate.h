7c2ff2985eeac16c32559d38d8581fe501a37239
Jacob Cho <jacobshcho@gmail.com>
2014-10-24 10:43:20 -0700

    Initial Commit

---
+++ CoverFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoverFlow
+//
+//  Created by Jacob Cho on 2014-10-24.
+//  Copyright (c) 2014 Jacob Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoverFlow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoverFlow
+//
+//  Created by Jacob Cho on 2014-10-24.
+//  Copyright (c) 2014 Jacob Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
